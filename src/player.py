501cae5efeca8d6236277b1a5260db7ea87f346e
NoRecV1 <rec0n.livegaming@gmail.com>
2020-01-07 17:56:18 +0100

    debug part1

--- src/player.py
+++ src/player.py
@@ -1,48 +1,55 @@
+
+import signal
+import time
+import sys
+from multiprocessing import Process
+
+
 class Player(Process):
-	def __init__(self, draw, mutex, id):
-		super.__init__()
-		self.mutex = mutex
-		self.draw = draw
-		self.id = id
-		self.hand = []
-		for i in range(0,5):
-			self.draw_card()
+    def __init__(self, draw, mutex, id):
+        super().__init__()
+        self.mutex = mutex
+        self.draw = draw
+        self.id = id
+        self.hand = []
+        for i in range(0,5):
+            self.draw_card()
 
-	def draw_card():
-		self.hand.append(self.draw.value.popleft())
+    def draw_card(self):
+        self.hand.append(self.draw.pop(0))
 
-	def handler(sig, frame):
-		if sig == SIGINT
-		if len(self.hand) == 0 :
-			print("Victoire")
-		else :
-			print("Défaite")
-		pause(5)
-		sys.exit()
+    def handler(self, sig, frame):
+        if sig == signal.SIGINT:
+            if len(self.hand) == 0:
+                print("Victoire")
+            else:
+                print("Défaite")
+            time.sleep(5)
+            sys.exit()
 
-	def display(state):
-		print(state)
-		print("Main :")
-		print(self.hand)
-		print(range(1, len(self.hand)))
-		print("Jouez !")
+    def display(self, state):
+        print(state)
+        print("Main :")
+        print(self.hand)
+        print(range(1, len(self.hand)))
+        print("Jouez !")
 
-	def run():
-		mq = MessageQueue(self.id)
-		while True:
-			state, c = mq.receive() #Après premier envoi du serveur (à affiner)
+    def run(self):
+        mq = MessageQueue(self.id)
+        while True:
+            state, c = mq.receive() #Après premier envoi du serveur (à affiner)
 
-			if not c:
-				draw_card()
+            if not c:
+                draw_card()
 
-			t = Thread(target = display, args = (state,))
-			t.start()
-			
-			k = wait_next_key_strike()
+            t = Thread(target = display, args = (state,))
+            t.start()
+            
+            k = wait_next_key_strike()
 
-			with mutex:
-				mq.send(self.hand[k-1])
+            with mutex:
+                mq.send(self.hand[k-1])
 
-			#Libération des autres process sans qu'ils ne jouent
+            #Libération des autres process sans qu'ils ne jouent
 
-			t.join()
+            t.join()
